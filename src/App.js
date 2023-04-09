a712674efdcfa9f1fd8edbf7b0f772dc1a10449b
Axelastach <axelpicaut@orange.fr>
2023-04-09 16:13:16 +0200

    petit test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 
 import './App.css';
+import Employe from './components/Employe';
 
 function App() {
+  console.log('we are about to list the employees');
+  let showEmployees =true;
+
   return (
     <div className="App">
-      <header className="App-header">
+      {console.log('inside the return ')}
+      {showEmployees ?
+      <>
+      <Employe/> 
+      <Employe/> 
+      <Employe/> 
+      <Employe/>
+      </>
+      :
+      <p>You cannot see the employees</p>
+    
+    }
+
         
-        <p>Hello !!!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+
     </div>
   );
 }
---
+++ src/components/Employe.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Employe() {
+    return (
+        <h3>Here's an employe</h3>
+        
+    )
+}
+
+export default Employe
