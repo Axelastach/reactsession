c9faa8f9a39be37ee1851af9ee97b16d0d866666
Axelastach <axelpicaut@orange.fr>
2023-04-09 17:03:15 +0200

    install Tailwind

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 
-import './App.css';
+import './index.css';
 import Employe from './components/Employe';
+import { useState } from 'react';
 
 function App() {
-  let showEmployees =true;
+  const [ role ,setRole]=useState('dev')
+  const showEmployees =true;
+  
 
   return (
-    <div className="App">
-      {console.log('inside the return ')}
-      {showEmployees ?
+    <div className="App bg-red-300">
+      
+      {showEmployees ?(
       <>
+      <input type ='text' onChange={(e)=>{
+        console.log(e.target.value)
+        setRole (e.target.value);
+      }}/>
       <Employe  name= "Axel" role = "Intern"/> 
-      <Employe name='Abey'/> 
+      <Employe name='Abey' role ={role}/> 
       <Employe name='Jphn'/> 
       
+      
       </>
-      :
-      <p>You cannot see the employees</p>
+       ) :
+     ( <p>You cannot see the employees</p>)
     
     }
 
