528b6249b56293084621931c398e44890efe77ce
Axelastach <axelpicaut@orange.fr>
2023-04-10 17:40:59 +0200

    add newdatabase

--- src/App.js
+++ src/App.js
@@ -2,14 +2,50 @@
 import './index.css';
 import Employe from './components/Employe';
 import { useState } from 'react';
+import{v4 as uuidv4} from 'uuid'
 
 function App() {
   const [ role ,setRole]=useState('dev')
+  const [employes,setEmployes]=useState(
+    [
+      {
+        
+        name:'Axel',
+        role:'developer',
+        img:"https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      },
+      {
+        name:'Caleb',
+        role:'engineer',
+        img:"https://images.pexels.com/photos/2709388/pexels-photo-2709388.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      },
+      {
+        name:'John',
+        role:'manager',
+        img:"https://images.pexels.com/photos/2380794/pexels-photo-2380794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      },
+      {
+        name:'Melanie',
+        role:'senior intern',
+        img:"https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      },
+      {
+        name:'Sal',
+        role:'software engineer',
+        img:"https://images.pexels.com/photos/2108843/pexels-photo-2108843.jpeg"
+      },
+      {
+        name:'Jake',
+        role:'the devops guy',
+        img:"https://images.pexels.com/photos/4556737/pexels-photo-4556737.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+      },
+    ]
+  )
   const showEmployees =true;
   
 
   return (
-    <div className="App bg-red-300">
+    <div className="App">
       
       {showEmployees ?(
       <>
@@ -17,10 +53,22 @@ function App() {
         console.log(e.target.value)
         setRole (e.target.value);
       }}/>
-      <Employe  name= "Axel" role = "Intern"/> 
-      <Employe name='Abey' role ={role}/> 
-      <Employe name='Jphn'/> 
+      <div className='flex flex-wrap justify-center'>
+        {employes.map((employee)=>{
+          
+        
+          return(
+          <Employe 
+          key={uuidv4()}
+          name={employee.name}
+          role={employee.role}
+          img={employee.img}
+          />
+
+        );})}
       
+ 
+      </div>
       
       </>
        ) :
